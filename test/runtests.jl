ebe4f1aa05112ac2ca6b2fca5c34017785ce27ca
Jay Weisskopf <jay@jayschwa.net>
2014-05-18 02:01:10 -0400

    Add window creation and destruction to test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,9 @@
 import GLFW
 
 GLFW.Init()
+window = GLFW.CreateWindow(640, 480, "GLFW.jl Test")
+GLFW.MakeContextCurrent(window)
+GLFW.SwapBuffers(window)
+GLFW.PollEvents()
+GLFW.DestroyWindow(window)
 GLFW.Terminate()
